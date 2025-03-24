31fbcccb3cebd8d8e14ae056b6739c335a8d8926
Glitch (glitch-blank-react) <none>
2025-03-24 01:10:36 +0000

    🛬📙 Checkpoint ./.glitch-assets:26469595/877 ./src/manifest.json:26469595/248 ./src/index.jsx:26469595/112

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Main from "./main.jsx"
 import {LayoutFunc} from "./database.jsx";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
+serviceWorkerRegistration.register();
 
 ReactDOM.render(
   <React.StrictMode>
